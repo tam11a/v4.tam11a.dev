85f115376dcebc66c7da23afdf96c4feaedef8f1
tam11a <ibrahimsadiktamim@gmail.com>
2025-12-02 19:53:31 -0600

    fixed spacing

--- src/scripts/smooth-scroll.ts
+++ src/scripts/smooth-scroll.ts
@@ -4,12 +4,12 @@ const init = () => {
   // creates the scrollSmoother
   ScrollSmoother.create({
     content: "#smooth-scroll-content",
+    smooth: 1.5, // Smoothness duration (seconds)
     smoothTouch: 0.1, // Slight smoothing on touch devices
     effects: true,
     normalizeScroll: true,
     autoResize: true,
     ease: "power1.out",
-    ignoreMobileResize: true,
   });
 };
 
